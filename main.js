94277522aa3b63b561ee0d618fc4a54f818b2295
Valeriya <valeriyalukovkina@gmail.com>
2022-07-14 18:04:48 +0800

    Change

--- main.js
+++ main.js
@@ -5,3 +5,20 @@
         elem.setAttribute('data-count', count++ + '.')
     }
 })('.block_title');
+
+;(function getNum(selectorFixed, selectorBlock){
+    let fixed = document.querySelectorAll(selectorFixed);
+    let block = document.querySelectorAll(selectorBlock);
+    for (let i = 0; i < block.length; i++) {
+        block[i].addEventListener('click', function() {
+            event.stopPropagation();
+        })
+        fixed[i].addEventListener('click', function() {
+            if (block[i].hasAttribute('open') === true) {
+                block[i].removeAttribute('open');
+            } else {
+                block[i].setAttribute('open', 'open');
+            }
+        });        
+    }
+})('.wrpp', '.block');
